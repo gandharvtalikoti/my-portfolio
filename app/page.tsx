80c653697e33607a9dd332f8930872110394accb
gandharv <gandharvwork@gmail.com>
2024-11-19 01:55:51 +0530

    fix: width and intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
+import Intro from '@/components/intro'
 import React from 'react'
 
 const Home = () => {
   return (
     <section className='py-24'>
-      <div className='container'>
-        <h1 className='text-xl font-bold'>Portfolio</h1>
+      <div className='container max-w-3xl'>
+        <Intro />
       </div>
-
     </section>
   )
 }
