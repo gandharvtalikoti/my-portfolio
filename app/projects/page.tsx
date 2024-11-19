01bbd5b02cd362722c6bfe4e50ddb806c9843b90
gandharv <gandharvwork@gmail.com>
2024-11-19 23:09:54 +0530

    refactor

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import Projects from '@/components/projects'
+import { getProjects } from '@/lib/projects'
 
-export default function ProjectsPage() {
-  return <h1>Adsadsadsad</h1>
+export default async function ProjectsPage() {
+  const projects = await getProjects()
+
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container max-w-3xl'>
+        <h1 className='title mb-12'>Projects</h1>
+
+        <Projects projects={projects} />
+      </div>
+    </section>
+  )
 }
