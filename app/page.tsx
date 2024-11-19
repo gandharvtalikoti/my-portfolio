5beecf3bd2b20706f53f983aac73c10978b232c4
gandharv <gandharvwork@gmail.com>
2024-11-19 19:26:52 +0530

    fix: dep versions solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Intro from '@/components/intro'
 import React from 'react'
+import { MDXRemote } from 'next-mdx-remote/rsc'
+
 
 const Home = () => {
+  const content = `# Hello, world!`
   return (
     <section className='py-24'>
       <div className='container max-w-3xl'>
         <Intro />
+        <MDXRemote source={content} />
       </div>
     </section>
   )
---
+++ app/posts/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Posts = () => {
+  return (
+    <div>fdasfd</div>
+  )
+}
+
+export default Posts
